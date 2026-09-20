2eee3ea3ab8efb868ef2f2e08d59e85954c79f93
Osmar Reyes <osmar.reyes@stedion>
2026-09-20 00:18:37 -0300

    feat: scaffold initial scenes

---
+++ scripts/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@export var SPEED:= 200.0
+@export var JUMP_VELOCITY:= -320.0
+
+@onready var sprite_2d = $Sprite2D
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction == -1:
+		sprite_2d.flip_h = true
+	if direction == 1:
+		sprite_2d.flip_h = false
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
