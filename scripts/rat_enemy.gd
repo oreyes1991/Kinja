ced7a5a73f4ac1b2b9fffac6d5157b73443235aa
Osmar Reyes <osmar.reyes@stedion>
2026-09-26 15:26:32 -0300

    feat: add rat audio

--- scripts/rat_enemy.gd
+++ scripts/rat_enemy.gd
@@ -1,13 +1,24 @@
 class_name rat_enemy
-extends RigidBody2D
+extends CharacterBody2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
+@onready var edge_detector: RayCast2D = $RayCast2D
+
+@export var direction: int = 1 
+@export var speed: float = 60.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animated_sprite_2d.play("walk")
-	pass # Replace with function body.
+
+func _physics_process(delta):
+	velocity.x = direction * speed
+	move_and_slide()
+
 # this is the kill collider
 func _on_area_2d_area_entered(area):
 	if (area.name == "foot_collider"):
+		audio_stream_player_2d.play()
+		await audio_stream_player_2d.finished
 		print("me electrocutaste pedrito!!!")
 		queue_free()
